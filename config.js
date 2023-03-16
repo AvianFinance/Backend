9d870c69b2906c7530a038feddd1d5e1be5ca31b
Meelan-98 <meelanbandara@gmail.com>
2023-03-16 12:02:58 +0530

    sell functions checked

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
-
 sexchange_token = "0x87402370A4D7391801565A3281DC9DFD5a9dDA7f"
 rexchange_token = "0x59ceeE702Db7A911b594F9eDa08A4b1fb8f44fB7"
 iexchange_token = "0xd045C3bCEA36F7A67B5e2C410Adcd82ab561C2Bc"
-rime_token   = "0x71146F50Cf97A5B2b8D66bc5bfF93b86Cd3FF1f1"
+rime_token   = "0x4909493F604AB882327ca880ad5B330e2B3C43C1"
 rime_rent = "0xA5e80F4980878b7C2c23D6fA002358A47d0060a3"
 
 module.exports = {
     sexchange_token,
     rexchange_token,
     iexchange_token,
-    rime_rent,
+    rime_token,
     rime_rent
 };
---
+++ faucet_sell.js
@@ -0,0 +1,66 @@
+const { ethers } = require("hardhat")
+const { mintNFT } = require('./scripts/mint_nft')
+const { pullProceeds } = require('./scripts/get_proceeds')
+const { ListNFT, UpdateListing, ViewASellListing, ViewSellListing, ViewSellListedAddrs, ViewSellListedAddrTokens, buyNFT} = require('./scripts/sexchange_functions')
+
+const provider = new ethers.providers.JsonRpcProvider("https://api.avax-test.network/ext/bc/C/rpc")
+const signer_m = new ethers.Wallet("7e0dd21cba3952c769b9a90376893a351d4ac356aeacd0e537f5022e08593528", provider); // Meelan Credentials
+const signer_r = new ethers.Wallet("2f3b47319ba27e3e58ae7a62ecb3966b23b9df1b8a12d1b7520f643a6d7fdc33", provider); // Rosy credentials
+const signer_i = new ethers.Wallet("986815db062b75efa84cd38ea93e08e9e13a42ee9493f756c1bc661d06201e68", provider); // Meelan Credentials
+
+stand = "ERC721" // token type : set correctly before initiating
+
+async function sexchange_handler(cond, signer){ // For handling buy sell related scenarios
+
+    if (cond==1){ // Mint a new NFT name
+        response =  await mintNFT("cute panda 2222","Beautiful panda","panda.jpg",signer,stand)
+        console.log(response)
+    }
+    else if (cond==2){ // list a nft to be sold in the market place
+        token_ID = 2
+        price = 0.06
+        response = await ListNFT(token_ID,price,signer,stand)
+        console.log(response)
+    }
+    else if (cond==3){ // view the price and the listing of a NFT
+        token_ID = 2
+        response = await ViewASellListing(token_ID,provider,stand)
+        console.log(response)
+    }
+    else if (cond==4){ // update the price of a NFT
+        token_ID = 2
+        price = 0.05
+        response = await UpdateListing(token_ID,price,signer,stand) 
+        console.log(response)
+    }
+    else if (cond==5){ // buy a listed nft
+        token_ID = 2
+        response = await buyNFT(token_ID,signer,stand) 
+        console.log(response)
+    }
+    else if (cond==6){ // pulls available proceeds
+        response = await pullProceeds(signer)
+        console.log(response)
+    }
+    else if (cond==7){ // view the set of sell listings
+        response = await ViewSellListing(provider)
+        console.log(response)
+    }
+    else if (cond==8){ // View the set of collection addresses listed for selling
+        response = await ViewSellListedAddrs(provider) 
+        console.log(response)
+    }
+    else if (cond==9){ // View the set of token ids listed under a given collection for selling
+        response = await ViewSellListedAddrTokens(stand,provider) 
+        console.log(response)
+    }
+}
+
+
+sexchange_handler(9,signer_r)
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
+
