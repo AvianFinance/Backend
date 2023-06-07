092e4faf3f6b809284b36dfca7835529186f5fd7
Isuru Ariyarathne <61895498+IsuruAriyarathne@users.noreply.github.com>
2023-06-07 14:15:03 +0530

    Update config

--- config.js
+++ config.js
@@ -13,6 +13,16 @@ installment_proxy_addr = "0xB5463C1418214088A722A845a88bFA72522A46Aa"
 rime_token   = "0x4909493F604AB882327ca880ad5B330e2B3C43C1"
 rime_rent = "0xA5e80F4980878b7C2c23D6fA002358A47d0060a3"
 
+//isuru
+// ins_impl_token = "0xcA34756bAbd1b49318955caF66D4acf993Ff70b1"
+// ins_proxy_token = "0xB388C5c0114AC0A05413B322386c9c8FE812Eb05"
+
+// rent_impl_token = "0x6D5525A975D3CF9A0Fe89abcF8D30aaf02E65434"
+// rent_proxy_token = "0x9bF19331CF09Eb6A693D78AeEb39A4E0C21113e1"
+
+// sell_impl_token = "0x3823f9B9ede8d22362a2D59A100aa99Fd615dB44"
+// sell_proxy_token = "0xCd54F936D1cE5cF9AffF5F56B4d3F3148bc114f9"
+
 module.exports = {
     sell_proxy_addr,
     rent_proxy_addr,
